70fd9af2924bd91fff20b3ab0de3cd4bdd523448
Gustavo-Meneses <90222051+Gustavo-Meneses@users.noreply.github.com>
2021-09-30 20:21:01 -0300

    onlycode.py
    somente o código da automação

--- onlycode.py
+++ onlycode.py
@@ -9,30 +9,21 @@ import time
 
 pyautogui.PAUSE = 1
 
- Passo 1 - Entrar no nosso sistema (entrar no link: Link que o arquivo a ser analisado está. )
 pyautogui.hotkey("ctrl", "t")
 link = "Link que o arquivo a ser analisado está"
 pyperclip.copy(link)
 pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
 
- Passo 2 - Navegar no sistema (entrar na pasta Exportar)
 time.sleep(5) - espera 5 segundos
 pyautogui.click(x=, y=, clicks=2)
 
- Passo 3 - baixar o arquivo de vendas
 time.sleep(2)
 pyautogui.click(x=, y=)
 pyautogui.click(x=, y=)
 pyautogui.click(x=, y=)
 time.sleep(3) - esperar ele fazer o download
 
- Vamos agora ler o arquivo baixado para pegar os indicadores
-
-- Faturamento
-- Quantidade de Produtos
-
-- Passo 4 - Calcular faturamento e quantidade de produtos vendidos
 import pandas as pd
 
 tabela = pd.read_excel(r"C:\Users\alonp\Downloads\Vendas - Dez.xlsx")
@@ -40,9 +31,7 @@ faturamento = tabela["Valor Final"].sum()
 quantidade = tabela["Quantidade"].sum()
 display(tabela)
 
-- VAMOS AGORA ENCAMINHAR UM EMAIL PELO GMAIL
 
-- Passo 5 - Enviar o email para a diretoria
 pyautogui.hotkey("ctrl", "t") - abre uma nova aba
 link = "https://mail.google.com/"
 pyperclip.copy(link)
